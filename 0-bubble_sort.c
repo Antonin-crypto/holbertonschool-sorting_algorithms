f37bb3a9620799dd090d226f931391235a0a69ab
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-04-09 14:08:12 +0000

    0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two elements of an array
+ * @a: Integer a
+ * @b: Integer b
+ */
+
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * bubble_sort - Doubly linked list node
+ * @array: Array of integers to print
+ * @size: Size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	if (size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			/*Echange si l'élément trouvé est supeur a l'element suivant*/
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
