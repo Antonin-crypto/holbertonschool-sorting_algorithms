3965774eaeb5f5ffadcd74b14d17c4f9bb67d410
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-04-10 07:52:17 +0000

    3-quick_sort.c

---
+++ 3-quick_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * swap - Swaps two elements of an array
+ * @a: Integer a
+ * @b: Integer b
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
