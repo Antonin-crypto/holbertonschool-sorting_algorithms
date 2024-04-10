ccf0003816bba93274b217380d42ab3c44f8fab3
antonin-crypto <akamis53@gmail.com>
2024-04-10 07:50:17 +0000

    creation 2-selection_sort.c

---
+++ 2-selection_sort.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+#include "stdlib.h"
+/**
+ * swap - Swaps two elements of an array
+ * @a: Integer a
+ * @b: Integer b
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+/**
+ * selection_sort - function that sorts an array of integers in ascending
+ * order using the Selection sort algorithm
+ * @size: size of the array
+ * @array: list with numbers
+ */
+void selection_sort(int *array, size_t size)
+{
+	 size_t i, index, minIndex;
+
+	 if (size < 2)
+		 return;
+	 for (i = 0; i < size - 1; i++)
+	 {
+		 minIndex = i;
+
+		 for (index = i + 1; index < size; index++)
+		 {
+			 if (array[index] < array[minIndex])
+			 {
+				 minIndex = index;
+			 }
+		 }
+		 if (minIndex != i)
+		 {
+			 swap(&array[i], &array[minIndex]);
+			 print_array(array, size);
+		 }
+	 }
+}
